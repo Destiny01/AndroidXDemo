4cb094a47078484188ae2db04ebc0dcb8ae94e60
平头哥 <1264915983@qq.com>
2018-12-04 18:31:49 +0800

    Rxjava操作符学习

--- rxjava/src/main/java/com/jinnuojiayin/rxjava/MainActivity.java
+++ rxjava/src/main/java/com/jinnuojiayin/rxjava/MainActivity.java
@@ -3,16 +3,23 @@ package com.jinnuojiayin.rxjava;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
@@ -35,7 +42,333 @@ public class MainActivity extends AppCompatActivity {
         //第二步：初始化 Observer
         //第三步：建立订阅关系
 
-        test06();
+        test23();
+    }
+
+    /**
+     * window
+     * 按照实际划分窗口，将数据发送给不同的 Observable
+     */
+    private void test23() {
+        Log.e(TAG, "window\n");
+        Observable.interval(1, TimeUnit.SECONDS) // 间隔一秒发一次
+                .take(15) // 最多接收15个
+                .window(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Observable<Long>>() {
+                    @Override
+                    public void accept(@NonNull Observable<Long> longObservable) throws Exception {
+                        Log.e(TAG, "Sub Divide begin...\n");
+                        longObservable.subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new Consumer<Long>() {
+                                    @Override
+                                    public void accept(@NonNull Long aLong) throws Exception {
+                                        Log.e(TAG, "Next:" + aLong + "\n");
+                                    }
+                                });
+                    }
+                });
+
+    }
+
+    /**
+     * scan
+     * scan 操作符作用和上面的 reduce 一致，唯一区别是 reduce 是个只追求结果的坏人，而 scan 会始终如一地把每一个步骤都输出。
+     */
+    private void test22() {
+        Observable.just(1, 2, 3)
+                .scan(new BiFunction<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Integer integer, @NonNull Integer integer2)
+                            throws Exception {
+                        return integer + integer2;
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(@NonNull Integer integer) throws Exception {
+                Log.e(TAG, "accept: reduce : " + integer + "\n");
+            }
+        });
+
+    }
+
+    /**
+     * reduce
+     * reduce 操作符每次用一个方法处理一个值，可以有一个 seed 作为初始值。
+     */
+    private void test21() {
+        Observable.just(1, 2, 3)
+                .reduce(new BiFunction<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Integer integer, @NonNull Integer integer2)
+                            throws Exception {
+                        return integer + integer2;
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(@NonNull Integer integer) throws Exception {
+                Log.e(TAG, "accept: reduce : " + integer + "\n");
+            }
+        });
+
+    }
+
+    /**
+     * merge
+     * merge 的作用是把多个 Observable 结合起来，接受可变参数，也支持迭代器集合。
+     * 注意它和 concat 的区别在于，不用等到 发射器 A 发送完所有的事件再进行发射器 B 的发送。
+     */
+    private void test20() {
+        Observable.merge(Observable.just(1, 2), Observable.just(3, 4, 5))
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        Log.e(TAG, "accept: merge :" + integer + "\n");
+                    }
+                });
+    }
+
+    /**
+     * last
+     * last 操作符仅取出可观察到的最后一个值，或者是满足某些条件的最后一项。
+     */
+    private void test19() {
+        Observable.just(1, 2, 3)
+                .last(4)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        Log.e(TAG, "last : " + integer + "\n");
+                    }
+                });
+    }
+
+    /**
+     * defer
+     * 简单地说就是每次订阅都会创建一个新的 Observable，并且如果没有被订阅，就不会产生新的 Observable。
+     */
+    private void test18() {
+        Observable<Integer> observable = Observable.defer(new Callable<ObservableSource<Integer>>
+                () {
+            @Override
+            public ObservableSource<Integer> call() throws Exception {
+                return Observable.just(1, 2, 3);
+            }
+        });
+        observable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                Log.e(TAG, "defer : " + integer + "\n");
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e(TAG, "defer : onError : " + e.getMessage() + "\n");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, "defer : onComplete\n");
+            }
+        });
+
+    }
+
+    /**
+     * debounce
+     * 去除发送频率过快的项
+     */
+    private void test17() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+// send events with simulated time wait
+                emitter.onNext(1); // skip
+                Thread.sleep(400);
+
+                emitter.onNext(2); // deliver
+                Thread.sleep(505);
+
+                emitter.onNext(3); // skip
+                Thread.sleep(100);
+
+                emitter.onNext(4); // deliver
+                Thread.sleep(605);
+
+                emitter.onNext(5); // deliver
+                Thread.sleep(510);
+
+                emitter.onComplete();
+            }
+        }).debounce(500, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.e(TAG, "debounce :" + integer + "\n");
+                    }
+                });
+    }
+
+    /**
+     * Single
+     * 顾名思义，Single 只会接收一个参数，而 SingleObserver 只会调用 onError() 或者 onSuccess()
+     */
+    private void test16() {
+        Single.just(new Random().nextInt())
+                .subscribe(new SingleObserver<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onSuccess(Integer integer) {
+                        Log.e(TAG, "single : onSuccess : " + integer + "\n");
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e(TAG, "single : onError : " + e.getMessage() + "\n");
+                    }
+                });
+    }
+
+    /**
+     * just
+     * just，没什么好说的，其实在前面各种例子都说明了，就是一个简单的发射器依次调用 onNext() 方法。
+     */
+    private void test15() {
+        Observable.just("1", "2", "3")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.e(TAG, "accept : onNext : " + s + "\n");
+                    }
+                });
+    }
+
+    /**
+     * take
+     * take，接受一个 long 型参数 count ，代表至多接收 count 个数据
+     */
+    private void test14() {
+        Flowable.fromArray(1, 2, 3, 4, 5)
+                .take(2)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.e(TAG, "accept: take : " + integer + "\n");
+                    }
+                });
+    }
+
+    /**
+     * skip
+     * skip 很有意思，其实作用就和字面意思一样，接受一个 long 型参数 count ，代表跳过 count 个数目开始接收
+     */
+    private void test13() {
+        Observable.just(1, 2, 3, 4, 5)
+                .skip(2)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.e(TAG, "skip : " + integer + "\n");
+                    }
+                });
+    }
+
+    /**
+     * doOnNext
+     * 其实觉得 doOnNext 应该不算一个操作符，但考虑到其常用性，我们还是咬咬牙将它放在了这里。
+     * 它的作用是让订阅者在接收到数据之前干点有意思的事情。假如我们在获取到数据之前想先保存一下它，无疑我们可以这样实现。
+     */
+    private void test12() {
+        Observable.just(1, 2, 3, 4)
+                .doOnNext(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.e(TAG, "doOnNext 保存 " + integer + "成功" + "\n");
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.e(TAG, "doOnNext :" + integer + "\n");
+            }
+        });
+    }
+
+    Disposable mDisposable;
+
+    /**
+     * interval
+     * interval 操作符用于间隔时间执行某个操作，其接受三个参数，分别是第一次发送延迟，间隔时间，时间单位。
+     */
+    private void test11() {
+        Log.e(TAG, "interval start : " + getNowStrTime() + "\n");
+        mDisposable = Observable.interval(3, 2, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        Log.e(TAG, "interval :" + aLong + " at " + getNowStrTime() + "\n");
+                    }
+                });
+    }
+
+    /**
+     * timer
+     * timer 很有意思，相当于一个定时任务。在 1.x 中它还可以执行间隔逻辑，但在 2.x 中此功能被交给了 interval，下一个会介绍。但需要注意的是，timer 和
+     * interval 均默认在新线程。
+     */
+    private void test10() {
+        Log.e(TAG, "timer start : " + getNowStrTime() + "\n");
+        Observable.timer(2, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        Log.e(TAG, "timer :" + aLong + " at " + getNowStrTime() + "\n");
+                    }
+                });
+    }
+
+    private String getNowStrTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd mm:ss");
+        return sdf.format(new Date(System.currentTimeMillis()));
+    }
+
+    /**
+     * buffer
+     * buffer 操作符接受两个参数，buffer(count,skip)，
+     * 作用是将 Observable 中的数据按 skip (步长) 分成最大不超过 count 的 buffer ，然后生成一个  Observable 。
+     */
+    private void test09() {
+        Observable.just(1, 2, 3, 4, 5)
+                .buffer(3, 2)
+                .subscribe(new Consumer<List<Integer>>() {
+                    @Override
+                    public void accept(List<Integer> integers) throws Exception {
+                        Log.e(TAG, "buffer size : " + integers.size() + "\n");
+                        Log.e(TAG, "buffer value : ");
+                        for (Integer i : integers) {
+                            Log.e(TAG, i + "");
+                        }
+                        Log.e(TAG, "\n");
+                    }
+                });
+
     }
 
     /**
@@ -164,7 +497,8 @@ public class MainActivity extends AppCompatActivity {
      * 最终接收器收到的事件数量是和发送器发送事件最少的那个发送器的发送事件数目相同，所以如截图中，5 很孤单，没有人愿意和它交往，孤独终老的单身狗。
      */
     private void test03() {
-        Observable.zip(getStringObservable(), getIntegerObservable(), new BiFunction<String, Integer, String>() {
+        Observable.zip(getStringObservable(), getIntegerObservable(), new BiFunction<String,
+                Integer, String>() {
             @Override
             public String apply(@NonNull String s, @NonNull Integer integer) throws Exception {
                 return s + integer;
@@ -298,5 +632,13 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "onComplete" + "\n");
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mDisposable != null && !mDisposable.isDisposed()) {
+            mDisposable.dispose();
+        }
     }
 }
